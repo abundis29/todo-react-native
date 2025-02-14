4cd6e193c80ce4238d16d3bf14e65864fe1e7a87
Ivan A <heysoyivan@gmail.com>
2025-02-15 01:43:37 +0900

    coverage 🚨

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,8 @@ module.exports = {
       "node_modules/(?!((jest-)?react-native|redux-persist|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|uuid)"
     ],
     collectCoverage: true,
+    coverageDirectory: './coverage',  // Ensures Jest outputs coverage files to ./coverage directory
+    coverageReporters: ['json-summary', 'text', 'lcov'],  // Adds more reporting formats
     collectCoverageFrom: [
       "**/*.{ts,tsx}",
       "!**/coverage/**",
